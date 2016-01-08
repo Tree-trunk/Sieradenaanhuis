25a8a6ea3d83df07f1e5416180f1f053847990ac
Tree-trunk <mailme@tree-trunk.nl>
2016-01-08 15:17:48 +0100

    javascript

--- main/js/jessica.js
+++ main/js/jessica.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
             if($('.product-options')[0]) { 
                 $('.product-options .intro.item').after('<div class="input-item paars">' +
                     '<p>' + 
-                    'Op voorraad, direct leverbaar <br />' + 
+                    '&#10003; Op voorraad, direct leverbaar <br />' + 
                     'Voor 15:00 besteld, morgen in huis' + 
                     '</p>' + 
                     '</div>');
